15350550e321a5026042225e90c887851bfdd43f
petra <petra.spirkova@apiumhub.com>
2019-04-26 22:09:21 +0200

    add components, configure routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {PortfolioComponent} from './portfolio/portfolio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'portfolio',
+    component: PortfolioComponent
+  },
+  {
+    path: '',
+    redirectTo: 'portfolio', pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: 'portfolio', pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { BuildingListComponent } from './building-list/building-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PortfolioComponent,
+    BuildingListComponent
   ],
   imports: [
     BrowserModule,
