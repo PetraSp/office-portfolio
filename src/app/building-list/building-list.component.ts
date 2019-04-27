8cefbf804470b4e9d0c5c69308c50a56daaff251
petra <petra.spirkova@apiumhub.com>
2019-04-27 15:27:01 +0200

    add data, add building service, create building names array

--- src/app/building-list/building-list.component.ts
+++ src/app/building-list/building-list.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import {BuildingService, IBuildingsJSON, IBuildingsData} from './building.service';
+
+export interface IBuilding {
+  name: string;
+  size: number;
+  usage: number;
+  capacity: number;
+}
 
 @Component({
   selector: 'app-building-list',
@@ -6,10 +14,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./building-list.component.css']
 })
 export class BuildingListComponent implements OnInit {
-
-  constructor() { }
+  buildings: IBuildingsJSON;
+  displayedColumns: string[] = ['Building', 'Size', 'Usage', 'Capacity'];
+  dataSource: IBuilding[] = [];
+  constructor(private buildingsService: BuildingService) { }
 
   ngOnInit() {
-  }
+    this.buildingsService.getBuildings().subscribe((data: IBuildingsJSON) => {
+      console.log(data.buildings);
+      const buildingNames = data.buildings.map(building => building.code);
+      console.log(buildingNames);
+      this.buildings = data;
 
+
+      for (let i = 0; i < data.buildings.length; i++) {
+        this.dataSource.push({
+            name: buildingNames[i],
+            size: 2, // buildingSizes[i];
+            usage: 3, // buildingUsages[i];
+            capacity: 4, // buildingCapacity[i];
+        });
+      }
+      console.log(this.dataSource);
+    });
+  }
 }
+
